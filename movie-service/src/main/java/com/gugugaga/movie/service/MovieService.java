34e30862ffa88bbccc2691cbbb04246bdbdf28c1
Kamil <ahmaadm84112@gmail.com>
2025-07-31 18:46:21 +0700

    feat(service): add MovieService to encapsulate movie business logic

---
+++ movie-service/src/main/java/com/gugugaga/movie/service/MovieService.java
@@ -0,0 +1,38 @@
+package com.gugugaga.movie.service;
+import com.gugugaga.movie.entity.Movie;
+import com.gugugaga.movie.repository.MovieRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+    public Optional<Movie> getMovieById(Long id) {
+        return movieRepository.findById(id);
+    }
+    public Movie createMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+    public void deleteMovie(Long id) {
+        movieRepository.deleteById(id);
+    }
+    public void softDeleteMovie(Long id) {
+        Movie movie = movieRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Tidak ada data yang ditemukan.")
+        );
+        movie.setDeleted(true);
+        movie.setDeletedAt(LocalDateTime.now());
+        movieRepository.save(movie);
+    }
+}
