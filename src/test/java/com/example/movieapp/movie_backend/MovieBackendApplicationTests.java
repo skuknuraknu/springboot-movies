1a9dc3888c2a8fca4062b0e71452208890dc16c4
Kamil <ahmaadm84112@gmail.com>
2025-07-31 18:38:11 +0700

    Init app

---
+++ src/test/java/com/example/movieapp/movie_backend/MovieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieapp.movie_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
