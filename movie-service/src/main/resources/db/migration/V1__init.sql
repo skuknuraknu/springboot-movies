f367f0df4a51326028248248bcc26d36e213bcdc
Kamil <ahmaadm84112@gmail.com>
2025-07-31 18:40:09 +0700

    db: add migration for create_movies_table

---
+++ movie-service/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tb_movies (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    genre VARCHAR(100),
+    release_year INT,
+    rating DECIMAL(2,1),
+    is_deleted boolean,
+);
