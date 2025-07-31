8aa57d90e81b8f3b456cfd9ee0dd150af7804a58
Kamil <ahmaadm84112@gmail.com>
2025-07-31 18:46:04 +0700

    feat(entity): add Movie.java entity

---
+++ movie-service/src/main/java/com/gugugaga/movie/entity/Movie.java
@@ -0,0 +1,83 @@
+package com.gugugaga.movie.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_movies")
+public class Movie {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Harap memasukkan title")
+    private String title;
+
+    @NotBlank(message = "Harap memasukkan genre")
+    private String genre;
+
+    @NotNull(message = "Harap memasukkan tahun rilis")
+    private int releaseYear;
+
+    @DecimalMin(value = "0.0", inclusive = true, message = "Rating cannot be less than 0.0")
+    @DecimalMax(value = "10.0", inclusive = true, message = "Rating cannot be more than 10.0")
+    private Double rating;
+
+    private Boolean isDeleted = false;
+    private LocalDateTime deletedAt;
+
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean delete) {
+        isDeleted = delete;
+    }
+    // Getters and setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+}
