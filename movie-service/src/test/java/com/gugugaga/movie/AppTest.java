6e3ccf5f0eee2d04e42e8f85a3e32b12da8b389c
Kamil <ahmaadm84112@gmail.com>
2025-07-31 18:43:58 +0700

    test: add AppTest

---
+++ movie-service/src/test/java/com/gugugaga/movie/AppTest.java
@@ -0,0 +1,14 @@
+package com.gugugaga.movie;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true, "If this fails, your career is over");
+    }
+}
