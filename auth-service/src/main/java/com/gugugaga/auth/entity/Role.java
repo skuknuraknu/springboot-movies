39b3419e1872536f2ca69d4bcd117b1f72392aa4
Kamil <ahmaadm84112@gmail.com>
2025-08-06 18:58:04 +0700

    Feat: Add getter and setter to Role entity

--- auth-service/src/main/java/com/gugugaga/auth/entity/Role.java
+++ auth-service/src/main/java/com/gugugaga/auth/entity/Role.java
@@ -25,4 +25,36 @@ public class Role {
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
