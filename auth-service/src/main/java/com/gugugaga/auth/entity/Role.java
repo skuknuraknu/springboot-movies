af7da324ee901580ca4965e868ab605608dc2ed1
Kamil <ahmaadm84112@gmail.com>
2025-08-09 09:02:13 +0700

    Refactor: Update Role entity to improve field annotations and data types

--- auth-service/src/main/java/com/gugugaga/auth/entity/Role.java
+++ auth-service/src/main/java/com/gugugaga/auth/entity/Role.java
@@ -1,35 +1,44 @@
 package com.gugugaga.auth.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.validation.constraints.NotBlank;
-
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_roles")
 public class Role {
-    @Id @GeneratedValue( strategy = GenerationType.IDENTITY )
-    private long id;
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @NotBlank(message = "Harap memasukkan nama role")
-    @Column(name = "name", unique = true, nullable = false)
+    @Size(max = 50, message = "Nama role maksimal 50 karakter")
+    @Column(name = "name", unique = true, nullable = false, length = 50)
     private String name;
 
-    @NotBlank(message = "Harap memasukkan deskripsi role")
-    @Column(name = "description")
+    @Size(max = 255, message = "Deskripsi role maksimal 255 karakter")
+    @Column(name = "description", length = 255)
     private String description;
 
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    public long getId() {
+    @OneToMany
+    private Set<UserRole> userRoles = new HashSet<>();
+    
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
