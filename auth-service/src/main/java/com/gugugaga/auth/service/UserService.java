2d22574fd98e431dc7ac684100db49a4d8bdcdb2
Kamil <ahmaadm84112@gmail.com>
2025-08-16 20:15:59 +0700

    Refactor: Update UserService to use CreateUserRequest DTO and enhance user validation during updates

--- auth-service/src/main/java/com/gugugaga/auth/service/UserService.java
+++ auth-service/src/main/java/com/gugugaga/auth/service/UserService.java
@@ -5,33 +5,47 @@ import java.util.Optional;
 import org.springframework.security.access.*;
 import org.springframework.stereotype.Service;
 
+import com.gugugaga.auth.dto.CreateUserRequest;
 import com.gugugaga.auth.dto.UpdateUserRequest;
 import com.gugugaga.auth.entity.User;
+import com.gugugaga.auth.mapper.UserMapper;
 import com.gugugaga.auth.repository.UserRepository;
 
 @Service
 public class UserService {
-
+    private final UserMapper userMapper;
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserMapper userMapper, UserRepository userRepository) {
+        this.userMapper = userMapper;
         this.userRepository = userRepository;
     }
-    public User createUser(User user) {
+    public User createUser(CreateUserRequest req) {
+        User user = userMapper.toEntityUser(req);
         user.setCreatedAt(LocalDateTime.now());
         return userRepository.save(user);
     }
     public User updateUser( Long userId, UpdateUserRequest req, Long requesterId ) {
-        if ( !userId.equals(requesterId) ) {
+        if ( !userId.equals(requesterId) )
             throw new AccessDeniedException("Tidak dapat memperbarui data user");
+        User existingUser = userRepository.findByIdAndIsActiveTrue(userId).orElseThrow(() -> new IllegalArgumentException("User dengan id " + userId + " tidak ditemukan."));
+        
+        if ( req.getEmail() != null && !req.getEmail().equals(existingUser.getEmail()) ) {
+            if ( userRepository.existsByEmailIgnoreCase( req.getEmail() ) ) {
+                throw new IllegalArgumentException("Email sudah terdaftar");
+            }
+            existingUser.setEmail(req.getEmail());
         }
-        User user = userRepository.findByIdAndIsActiveTrue(userId)
-                .orElseThrow(() -> new IllegalArgumentException("User dengan id " + userId + " tidak ditemukan."));
-        user.setUsername(req.getUsername());
-        user.setEmail(req.getEmail());
-        user.setUpdatedAt(LocalDateTime.now());
-        return userRepository.save(user);
-    }
+        if ( req.getUsername() != null && !req.getUsername().equals(existingUser.getUsername()) ) {
+            if ( userRepository.existsByUsernameIgnoreCase( req.getUsername() ) ) {
+                throw new IllegalArgumentException("Username sudah terdaftar");
+            }
+            existingUser.setUsername(req.getUsername());
+        }
+        userMapper.updateEntityFromDto(req, existingUser);
+        existingUser.setUpdatedAt(LocalDateTime.now());
+        return userRepository.save(existingUser);
+    }   
     public void deleteUser(Long id) {
         User user = userRepository.findByIdAndIsActiveTrue(id).orElseThrow(() -> new IllegalArgumentException("User dengan id " + id + " tidak ditemukan."));
         user.setUpdatedAt(LocalDateTime.now());
@@ -42,7 +56,7 @@ public class UserService {
         return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("User dengan id " + id + " tidak ditemukan."));
     }
     // Check if email exists
-    public Optional<String> findValidationError( User user ) {
+    public Optional<String> findValidationError( CreateUserRequest user ) {
         if ( userRepository.existsByUsernameIgnoreCase( user.getUsername() ) ) {
             return Optional.of( "Username sudah terdaftar" );
         }
