d24bed21ac17f79799f7121022d0cd0bcb332a92
Kamil <ahmaadm84112@gmail.com>
2025-08-28 19:02:08 +0700

    feat: Implement rate limiting filter and configuration

---
+++ auth-service/src/main/java/com/gugugaga/auth/config/RateLimitingConfig.java
@@ -0,0 +1,20 @@
+package com.gugugaga.auth.config;
+
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
+
+@Configuration
+public class RateLimitingConfig {
+    @Bean
+    public Bucket bucket(){
+        Bandwidth limit = Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(1)));
+        return Bucket.builder().addLimit(limit).build();
+    }
+}
