b0c38fe68bdf16a4c6ce8774fd619901ad9de13e
Kamil <ahmaadm84112@gmail.com>
2025-08-28 11:33:20 +0700

    feat: Add method to find movies by ID in MovieRepository

--- movie-service/src/main/java/com/gugugaga/movie/repository/MovieRepository.java
+++ movie-service/src/main/java/com/gugugaga/movie/repository/MovieRepository.java
@@ -1,7 +1,12 @@
 package com.gugugaga.movie.repository;
 import com.gugugaga.movie.entity.Movie;
+
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findById(long id);
 
 }
