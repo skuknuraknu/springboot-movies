15bae030c1370cb24ac057c8c2ae894ce00eca59
Kamil <ahmaadm84112@gmail.com>
2025-08-30 12:38:06 +0700

    feat: Create UserSubscriptionResponse DTO for managing subscription limits

---
+++ auth-service/src/main/java/com/gugugaga/auth/dto/UserSubscriptionResponse.java
@@ -0,0 +1,14 @@
+package com.gugugaga.auth.dto;
+
+public class UserSubscriptionResponse {
+    private Long limit;
+
+    public Long getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Long limit) {
+        this.limit = limit;
+    }
+    
+}
